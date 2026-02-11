2da4232db07f75059b3fc7592ac30855d33e0968
emersonpisoni <emersonpisoni14@gmail.com>
2026-02-11 10:36:23 -0300

    add callback example

---
+++ src/callback.js
@@ -0,0 +1,23 @@
+getUser(id, (err, user) => {
+  if (err) return handle(err)
+
+  getOrders(user, (err, orders) => {
+    if (err) return handle(err)
+
+    getInvoice(orders, (err, invoice) => {
+      if (err) return handle(err)
+
+      send(invoice)
+    })
+  })
+})
+
+// nesting depth grows with each step
+
+// duplicated error checks
+
+// hard to rearrange steps
+
+// hard to reuse intermediate logic
+
+// control flow tied to indentation
