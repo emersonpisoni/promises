021e986fc1cea1cff9f8a79289b9cdbc20ea5453
emersonpisoni <emersonpisoni14@gmail.com>
2026-03-03 10:17:31 -0300

    promise race

---
+++ src/interfaces/promise.race.js
@@ -0,0 +1,13 @@
+Promise.race([
+  task("Slow", 3000),
+  task("Fast", 1000)
+])
+  .then(result => {
+    console.log("Winner:", result);
+  })
+  .catch(err => {
+    console.error("Error:", err);
+  });
+
+// runs all tasks in parallel, and resolves or rejects as soon as the first task finishes.
+// the first task to complete (either success or failure)
